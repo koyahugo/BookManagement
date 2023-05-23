9c8b9e2a03f5354de729152f3ddaae9407043122
BuildTools <unconfigured@null.spigotmc.org>
2023-05-23 23:20:57 +0900

    フレーム遷移初期設定

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ class BookApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self._frame = None
-        self.switch_frame(StartPage)
+        self.switch_frame(MainPage)
 
     def switch_frame(self, frame_class):
         new_frame = frame_class(self)
@@ -12,7 +12,7 @@ class BookApp(tk.Tk):
             self._frame.destroy()
         self._frame = new_frame
         self._frame.pack()
-
+"""
 class StartPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
@@ -24,9 +24,11 @@ class PageOne(tk.Frame):
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Page one", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=10)
         tk.Button(self, text="Go to start page",command=lambda: master.switch_frame(StartPage)).pack()
+"""
 
 class MainPage(tk.Frame):
-    def __init__(self, master):
+    def __init__(self, master: BookApp):
+        self.master = master
         tk.Frame.__init__(self, master)
         #self.grid(sticky="nsew")
         self.create_widgets()
@@ -43,17 +45,34 @@ class MainPage(tk.Frame):
 
     def user_registration(self):
         print("ユーザー登録がクリックされました")
+        self.master.switch_frame(UserPage)
 
     def book_list(self):
         print("図書一覧がクリックされました")
+        self.master.switch_frame(BookListPage)
 
     def borrow(self):
         print("貸出がクリックされました")
+        self.master.switch_frame(BookListPage)
 
     def quit(self):
         print("終了がクリックされました")
 
+class UserPage(tk.Frame):
+    def __init__(self,master:BookApp):
+        print(type(master))
+        tk.Frame.__init__(self, master)
+        tk.Button(self,text="メインページに戻る",command=lambda: master.switch_frame(MainPage)).pack()
 
+class BookListPage(tk.Frame):
+    def __init__(self,master:BookApp):
+        tk.Frame.__init__(self, master)
+        tk.Button(self,text="メインページに戻る",command=lambda: master.switch_frame(MainPage)).pack()
+
+class BorrowPage(tk.Frame):
+    def __init__(self, master:BookApp):
+        tk.Frame.__init__(self,master)
+        tk.Button(self,text="メインページに戻る",command=lambda: master.switch_frame(MainPage)).pack()
 
 
 if __name__ == "__main__":
